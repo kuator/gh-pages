1e2a7ae7328818c40c20a2b685d302546adcaf51
Kuat <kuatabylkasymov@gmail.com>
2020-03-24 22:52:09 +0600

    update

--- coveranimation/script.js
+++ coveranimation/script.js
@@ -2,15 +2,15 @@ function ready(fn) {
   if (document.readyState != 'loading'){
     fn();
   } else {
+    document.body.style = 'display:none;color: black;'
     document.write('Loading');
-    document.body.style = 'display:none;'
     document.addEventListener('DOMContentLoaded', fn);
   }
 }
 
 function myFunction() {
     ready(function() {
-      // document.body.style = 'display:block;'
+      document.body.style = 'display:block;display:none;color: initial;'
     })
 }
 
