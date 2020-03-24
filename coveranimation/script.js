8fd37c1d064605443978b0b9270d05943c635acc
Kuat <kuatabylkasymov@gmail.com>
2020-03-24 23:10:48 +0600

    update

--- coveranimation/script.js
+++ coveranimation/script.js
@@ -1,5 +1,4 @@
 console.log('asdfasdf');
-document.body.style = 'display:none;color: black;'
 
 window.addEventListener('load', function () {
   document.body.style = 'display:block;color: initial;'
